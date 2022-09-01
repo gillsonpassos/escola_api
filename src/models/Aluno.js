851fc4852fa20ac4a77d43762159a80bb089a8f3
gillsonpassos <oliveirapassos07@outlook.com>
2022-09-01 13:04:51 -0300

    Listando alunos com suas fotos

--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -10,7 +10,7 @@ export default class Aluno extends Model {
           validate: {
             len: {
               args: [3, 255],
-              msg: "Nome precisa ter entre 3 e255 caracteres",
+              msg: "Nome precisa ter entre 3 e 255 caracteres",
             },
           },
         },
@@ -20,7 +20,7 @@ export default class Aluno extends Model {
           validate: {
             len: {
               args: [3, 255],
-              msg: "Sobrenome precisa ter entre 3 e255 caracteres",
+              msg: "Sobrenome precisa ter entre 3 e 255 caracteres",
             },
           },
         },
@@ -69,5 +69,9 @@ export default class Aluno extends Model {
       }
     );
     return this;
+  }
+
+  static associate(models) {
+    this.hasMany(models.Foto, { foreignKey: "aluno_id" });
   }
 }
